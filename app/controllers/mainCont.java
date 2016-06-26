bfe8e9293e915e2898da1f56eb9bef7de7ad2f6d
Your Name <you@example.com>
2016-06-26 14:58:50 +0300

    GOOD AGAIN

--- app/controllers/mainCont.java
+++ app/controllers/mainCont.java
@@ -51,8 +51,9 @@ public class mainCont extends Controller {
 			}
 	
 			try {
-				con = DriverManager.getConnection(
-						"jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440", "judorsa_1440", "dvpuX2KrnZDJAoI--T5u");
+				con = DriverManager.getConnection("jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440?user=judorsa_1440&password=dvpuX2KrnZDJAoI--T5u&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory");
+			//	con = DriverManager.getConnection(
+			//			"jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440", "judorsa_1440", "dvpuX2KrnZDJAoI--T5u");
 			//	con = DriverManager.getConnection(
 			//			"jdbc:postgresql://127.0.0.1:10000/judorsa_1440", "judorsa_1440", "dvpuX2KrnZDJAoI--T5u");
 			/*
