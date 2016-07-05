693cbd541a3ca34997c65bc93d915984592b5f54
Your Name <you@example.com>
2016-07-05 13:50:30 +0300

    cool

--- app/controllers/mainCont.java
+++ app/controllers/mainCont.java
@@ -57,14 +57,14 @@ public class mainCont extends Controller {
 				props.setProperty("user", "judorsa_1440");
 				props.setProperty("password", "dvpuX2KrnZDJAoI--T5u");
 				props.setProperty("sslmode", "disable");
-				con = DriverManager.getConnection(
-						"jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440", props);
+		//		con = DriverManager.getConnection(
+		//				"jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440", props);
 				//con = DriverManager.getConnection(
 				//		"jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440", "judorsa_1440", "dvpuX2KrnZDJAoI--T5u");
 	//			con = DriverManager.getConnection(
 	//					"jdbc:postgresql://judorsa-1440.postgresql.dbs.appsdeck.eu:30556/judorsa_1440?user=judorsa_1440&password=dvpuX2KrnZDJAoI--T5u&ssl=false");
-		//	con = DriverManager.getConnection(
-		//				"jdbc:postgresql://127.0.0.1:10000/judorsa_1440", props);
+			con = DriverManager.getConnection(
+						"jdbc:postgresql://127.0.0.1:10000/judorsa_1440", props);
 			/*
 			 */
 			//	con = DriverManager.getConnection(
